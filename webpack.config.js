cc5f2cf9d4d0ac224bc7d54ac059a8ed910e4fee
Matt Palumbo <matt.m.palumbo@gmail.com>
2018-08-31 08:51:28 -0400

    fixed dependency issue, added build script

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		main: "./src/js"
+	},
+	mode: "production",
+	output: {
+		filename: "[name]-bundle.js",
+		path: path.resolve(__dirname, "./dist")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	}
+}
